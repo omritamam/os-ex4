50a3fa5167cebbf0f11a2427d71537b12804b014
Omri Tamam <omri.tamam@cs.huji.ac.il>
2022-05-23 20:27:51 +0300

    done!

--- VirtualMemory.h
+++ VirtualMemory.h
@@ -10,13 +10,14 @@ struct PageInfo{
     uint64_t pageNum;
     int frameNum;
     int layer;
-    long long int fatherLinkAddress;
+    int fatherFrame;
+    int pageIndInFatherPage;
 };
 
 uint64_t translateAdress(uint64_t virtualAddress);
-int findNewFrame(uint64_t virtualAddress);
-PageInfo choosePageToEvict(PageInfo curFrame, uint64_t destination);
-PageInfo comparePagesDestinations(PageInfo *p1, PageInfo *p2, uint64_t destination);
+int findNewFrame(uint64_t virtualAddress, int lookingFrame);
+PageInfo choosePageToEvict(PageInfo curFrame, uint64_t destination, int lookingFrame);
+PageInfo comparePagesDestinations(PageInfo *p1, PageInfo *p2, uint64_t destination, int lookingFrame);
 int biggestFrameNum(int curFrameInd, int layer);
 void fillFrameWithZeros(int frameInd);
 
@@ -61,17 +62,25 @@ uint64_t translateAdress(uint64_t virtualAddress){
     //travel down im tables tree
     word_t nextTable;
     word_t currentTable = 0;
-    for (int layer = 0 ; layer < TABLES_DEPTH; layer++){
+    for (int layer = 0 ; layer < TABLES_DEPTH-1; layer++){
         PMread(currentTable * PAGE_SIZE + chunks[layer], &nextTable);
         if(nextTable == 0){
-            auto newFrame = findNewFrame(dest);
+            auto newFrame = findNewFrame(dest, currentTable);
             fillFrameWithZeros(newFrame);
             PMwrite(currentTable * PAGE_SIZE + chunks[layer], newFrame);
             nextTable = newFrame;
         }
         currentTable = nextTable;
     }
-    PMrestore(nextTable, dest);
+
+    PMread(currentTable * PAGE_SIZE + chunks[TABLES_DEPTH-1], &nextTable);
+    if(nextTable == 0){
+        auto newFrame = findNewFrame(dest, currentTable);
+        PMrestore(newFrame, dest);
+        PMwrite(currentTable * PAGE_SIZE + chunks[TABLES_DEPTH-1], newFrame);
+        nextTable = newFrame;
+    }
+
     auto offset = chunks[TABLES_DEPTH];
     return (nextTable<<OFFSET_WIDTH) + offset;
 
@@ -86,7 +95,7 @@ void fillFrameWithZeros(int frameInd) {
 }
 
 
-int findNewFrame(uint64_t virtualAddress){
+int findNewFrame(uint64_t virtualAddress, int lookingFrame){
     //CHECK RAM NOT FULL
     auto biggestFrame = biggestFrameNum(0,0);
     if(biggestFrame != NUM_FRAMES - 1){
@@ -94,9 +103,12 @@ int findNewFrame(uint64_t virtualAddress){
     }
     // RAM IS FULL
     PageInfo rootPage = {0,0,0,0};
-    PageInfo chosenPage = choosePageToEvict(rootPage,virtualAddress);
-    PMevict(chosenPage.frameNum,chosenPage.pageNum);
-    PMwrite(chosenPage.fatherLinkAddress,0);
+    PageInfo chosenPage = choosePageToEvict(rootPage,virtualAddress, lookingFrame);
+    if(chosenPage.layer == TABLES_DEPTH){
+        PMevict(chosenPage.frameNum,chosenPage.pageNum);
+    }
+    fillFrameWithZeros(chosenPage.frameNum);
+    PMwrite(chosenPage.fatherFrame * PAGE_SIZE + chosenPage.pageIndInFatherPage,0);
     return chosenPage.frameNum;
 }
 
@@ -119,7 +131,7 @@ int biggestFrameNum(int curFrameInd, int layer) {
 }
 
 
-PageInfo choosePageToEvict(PageInfo curFrame, uint64_t destination) {
+PageInfo choosePageToEvict(PageInfo curFrame, uint64_t destination, int lookingFrame) {
     //Check if the page is a leaf
     if(curFrame.layer == TABLES_DEPTH){
         return curFrame;
@@ -145,20 +157,18 @@ PageInfo choosePageToEvict(PageInfo curFrame, uint64_t destination) {
         if(value == 0){
             continue;
         }
-        long long int x;
-        x = curFrame.frameNum * PAGE_SIZE + ind;
-        PageInfo curChild = { (curFrame.pageNum<<OFFSET_WIDTH)+ind, value , curFrame.layer+1, x};
-        auto curResult = choosePageToEvict(curChild, destination);
-        bestResult = comparePagesDestinations(&curResult, &bestResult, destination);
+        PageInfo curChild = { (curFrame.pageNum<<OFFSET_WIDTH)+ind, value , curFrame.layer+1, curFrame.frameNum, ind};
+        auto curResult = choosePageToEvict(curChild, destination, lookingFrame);
+        bestResult = comparePagesDestinations(&curResult, &bestResult, destination, lookingFrame);
         }
     return bestResult;
     }
 
-PageInfo comparePagesDestinations(PageInfo *p1, PageInfo *p2, uint64_t destination) {
-    if(p1->frameNum==0){
+PageInfo comparePagesDestinations(PageInfo *p1, PageInfo *p2, uint64_t destination, int lookingFrame) {
+    if(p1->frameNum==0 || p1->frameNum == lookingFrame){
         return *p2;
     }
-    if(p2->frameNum==0){
+    if(p2->frameNum==0|| p2->frameNum == lookingFrame){
         return *p1;
     }
     uint64_t d1 = destination - p1->pageNum;
@@ -167,7 +177,13 @@ PageInfo comparePagesDestinations(PageInfo *p1, PageInfo *p2, uint64_t destinati
     d1 = destination - p2->pageNum;
     d2 = NUM_PAGES - (destination - p2->pageNum);
     auto p2Score = std::min(d1,d2);
-    if(p1Score < p2Score){
+    if(p1->layer < p2->layer){
+        return *p1;
+    }
+    if(p1->layer > p2->layer){
+        return *p2;
+    }
+    if(p1Score > p2Score){
         return *p1;
     }
     return *p2;
